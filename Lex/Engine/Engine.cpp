9296cfe2f1d65517367ceee757ffd856873b750d
CodeWizatd <codewizardcompany@gmail.com>
2025-02-17 09:09:34 +0100

    Setup WinMain

--- Lex/Engine/Engine.cpp
+++ Lex/Engine/Engine.cpp
@@ -1,9 +1,14 @@
 ﻿// Engine.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
 
-#include <iostream>
+#include <Windows.h>
 
-int main()
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
-    std::cout << "Hello World!\n";
+
+    MessageBox(NULL, L"Hello, World!", L"Sample", MB_OK);
+
+
+
+    return 0;
 }
